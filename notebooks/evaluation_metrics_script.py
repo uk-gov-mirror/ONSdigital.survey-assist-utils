bac245d343a70c8bb99ad783c8413598d8ddb501
marklondon999 <181754572+marklondon999@users.noreply.github.com>
2025-06-19 10:53:20 +0000

    feat(evaluation): linting improved running script evaluation_metrics_script.py

--- notebooks/evaluation_metrics_script.py
+++ notebooks/evaluation_metrics_script.py
@@ -12,32 +12,34 @@
 #     name: python3
 # ---
 
-import importlib
+# %%
+"""Temporary runner script to test the coder_alignment."""
+
 
 # %%
 from pathlib import Path
 
 import pandas as pd
 
-from survey_assist_utils.evaluation import coder_alignment
-from survey_assist_utils.evaluation.coder_alignment import LabelAccuracy
-
-importlib.reload(coder_alignment)
+from survey_assist_utils.evaluation.coder_alignment import ColumnConfig, LabelAccuracy
 
 # %%
 # Set up some lists to hold the columns we are interested in:
-test_environment = True
+TEST_ENVIRONMENT = True
 
-if test_environment:
-    coders_list = ["CC_1", "CC_2", "CC_3"]
-    candidate_list = ["SA_1", "SA_2", "SA_3", "SA_4", "SA_5"]
-    candidate_likelihood = [
-        "SA_score_1",
-        "SA_score_2",
-        "SA_score_3",
-        "SA_score_4",
-        "SA_score_5",
-    ]
+if TEST_ENVIRONMENT:
+    col_config = ColumnConfig(
+        clerical_label_cols=["CC_1", "CC_2", "CC_3"],
+        model_label_cols=["SA_1", "SA_2", "SA_3", "SA_4", "SA_5"],
+        model_score_cols=[
+            "SA_score_1",
+            "SA_score_2",
+            "SA_score_3",
+            "SA_score_4",
+            "SA_score_5",
+        ],
+        id_col="unique_id",
+    )
 
     # Define the directory and file list
     test_directory = Path("/home/user/survey-assist-utils/data/artificial_data")
@@ -50,29 +52,31 @@ if test_environment:
     ]
 
     # Select the file under test
-    file_under_test = file_test_list[4]
 
     # Construct the full path to the file
-    file_path = test_directory / file_under_test
+    file_path = test_directory / file_test_list[4]
 
     # Read the CSV file with all data as strings
     process_data = pd.read_csv(file_path, dtype=str)
 else:
-    candidate_list = [
-        "candidate_1_sic_code",
-        "candidate_2_sic_code",
-        "candidate_3_sic_code",
-        "candidate_4_sic_code",
-        "candidate_5_sic_code",
-    ]
-    candidate_likelihood = [
-        "candidate_1_likelihood",
-        "candidate_2_likelihood",
-        "candidate_3_likelihood",
-        "candidate_4_likelihood",
-        "candidate_5_likelihood",
-    ]
-    coders_list = ["sic_ind_occ1", "sic_ind_occ2", "sic_ind_occ3"]
+    col_config = ColumnConfig(
+        model_label_cols=[
+            "candidate_1_sic_code",
+            "candidate_2_sic_code",
+            "candidate_3_sic_code",
+            "candidate_4_sic_code",
+            "candidate_5_sic_code",
+        ],
+        model_score_cols=[
+            "candidate_1_likelihood",
+            "candidate_2_likelihood",
+            "candidate_3_likelihood",
+            "candidate_4_likelihood",
+            "candidate_5_likelihood",
+        ],
+        clerical_label_cols=["sic_ind_occ1", "sic_ind_occ2", "sic_ind_occ3"],
+        id_col="unique_id",
+    )
     process_data = pd.read_csv(
         "/home/user/survey-assist-utils/data/evaluation_data/combined_outputs.csv",
         dtype=str,
@@ -80,26 +84,18 @@ else:
 
 print("process_data", process_data.shape)
 
-# %%
-# Create the object to work on with the analysis:
-analyzer = LabelAccuracy(
-    process_data,
-    id_col="unique_id",
-    model_label_cols=candidate_list,
-    model_score_cols=candidate_likelihood,
-    clerical_label_cols=coders_list,
-)
+analyzer = LabelAccuracy(df=process_data, column_config=col_config)
 
 # %%
 # Basic accuracy
 digit_list = ["2-digit", "full"]
-type_digits = digit_list[0]
+TYPE_DIGITS = digit_list[0]
 print(
-    f"Overall accuracy {type_digits},  {analyzer.get_accuracy(threshold = 0.0, match_type = type_digits):.1f}%"
+    f"Overall accuracy {TYPE_DIGITS},  {analyzer.get_accuracy(match_type = TYPE_DIGITS):.1f}%"
 )
-type_digits = digit_list[1]
+TYPE_DIGITS = digit_list[1]
 print(
-    f"Overall accuracy {type_digits},  {analyzer.get_accuracy(threshold = 0.0, match_type = type_digits):.1f}%"
+    f"Overall accuracy {TYPE_DIGITS},  {analyzer.get_accuracy(match_type = TYPE_DIGITS):.1f}%"
 )
 
 # %%
@@ -115,8 +111,8 @@ analyzer.plot_threshold_curves()
 analyzer.get_summary_stats()
 
 # %%
-human_code_col = coders_list[0]
-llm_code_col = candidate_list[0]
+human_code_col = col_config.clerical_label_cols[0]
+llm_code_col = col_config.model_label_cols[0]
 exclude_patterns = ["x", "-9"]
 analyzer.plot_confusion_heatmap(
     human_code_col=human_code_col,
