21384ecc1332476cebea63cac20cffba4be6dfcd
Luke Roantree <206716137+lukeroantreeONS@users.noreply.github.com>
2025-07-04 15:26:21 +0000

    chore(utils): correct the module structure for synthetic-responses

--- src/survey_assist_utils/synthetic_responses/__init__.py
+++
@@ -1,7 +0,0 @@
-from .prompts import _persona_prompt, _reminder_prompt
-from .synthetic_response_utils import (
-    answer_followup,
-    construct_prompt,
-    get_followup,
-    instantiate_llm,
-)
