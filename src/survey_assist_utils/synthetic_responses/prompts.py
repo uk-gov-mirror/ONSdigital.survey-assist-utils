ab18014e19a5e5eb14137a779313dc6f54a3751b
Luke Roantree <206716137+lukeroantreeONS@users.noreply.github.com>
2025-07-21 12:33:50 +0000

    chore(synthetic_responses): restructure for more minimal PR - add documentation, remove tests, remove data access functions, add example script

--- src/survey_assist_utils/synthetic_responses/prompts.py
+++ src/survey_assist_utils/synthetic_responses/prompts.py
@@ -1,37 +1,75 @@
-def _persona_prompt(persona):
-    """Constructs a section of the LLM prompt, informing it about
+"""This module contains prompt component templates, and helper functions for constructing prompt templates, 
+which enable generation of synthetic responses to survey questions.
+
+This module is currently limited to a template and template constructor to request an LLM to answer a
+SIC follow-up question.
+"""
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts.prompt import PromptTemplate
+
+from .response_models import FollowupAnswerResponse, FollowupAnswerRequest
+
+def _persona_prompt(persona) -> str:
+    """Constructs a section of the LLM prompt template, informing it about
     the personal characteristics (if any) of the persona it should
     emulate.
     """
     if persona is None:
         return """
-        You are a UK worker, responding to a survey surrounding industrial
-        classification for the use in the UK official statistics.
-        You always respond in British English."""
-
+               You are a UK worker, responding to a survey surrounding industrial
+               classification for the use in the UK official statistics.
+               You always respond in British English.
+               """
     else:
         # TODO
-        pass
+        return ""
 
 
-def _reminder_prompt(request_body: dict, followup_question: str):
-    """Constructs a section of the LLM prompt which 'reminds' it about the job
-    held by the person it is pretending to be, and asks it to respond to the
-    provided follow-up question.
+_reminder_template = """
+Below is a reminder of the main activity your
+company does, your job title and job description.
+The survey interviewer has not been able to fully classify your company's
+Standard Industrial Classification (SIC) from the Job Data you have provided so far,
+and so they have asked you a clarifying question.
+Please answer this clarifying question, using the Output Format specified below.
+
+===Output Format===
+{format_instructions}
+
+===Your Job Data===
+- Company's main activity: {org_description}
+- Job Title: {job_title}
+- Job Description: {job_description}
+
+===Survey Data===
+- clarifying question: {followup_question}
+"""
+
+def make_followup_answer_prompt_pydantic(persona, request_body: dict, followup_question: str):
+    """Constructs a prompt for answering a follow-up question, formatted for a Pydantic output.
+
+    Args:
+        persona (TODO): An object describing the characteristics of the persona to emulate. 
+        request_body (dict): A dictionary containing the survey response data, including 
+                             "org_description", 
+                             "job_title",
+                             "job_description".
+        followup_question (str): The clarifying question from the survey interviewer.
+
+    Returns:
+        PromptTemplate: A Langchain PromptTemplate ready to be used with an LLM.
     """
-    return f"""
-    Below is a reminder of the main activity your
-    company does, your job title and job description.
-    The survey interviewer has not been able to fully classify your company's
-    Standard Industrial Classification (SIC) from the Job Data you have provided so far,
-    and so they have asked you a clarifying question.
-    Please answer this clarifying question.
-
-    ===Your Job Data===
-    - Company's main activity: {request_body['org_description']}
-    - Job Title: {request_body['job_title']}
-    - Job Description: {request_body['job_description']}
-
-    ===Survey Data===
-    - clarifying question: {followup_question}
-    """
+    parser = PydanticOutputParser(
+        pydantic_object=FollowupAnswerResponse
+    )
+    persona_prompt = _persona_prompt(persona)
+    return PromptTemplate.from_template(
+                template=persona_prompt + _reminder_template,
+                partial_variables={
+                    "format_instructions": parser.get_format_instructions(),
+                    "org_description": request_body["industry_descr"],
+                    "job_title": request_body["job_title"],
+                    "job_description": request_body["job_description"],
+                    "followup_question": followup_question,
+                },
+    )
--- src/survey_assist_utils/synthetic_responses/synthetic_response_utils.py
+++ src/survey_assist_utils/synthetic_responses/synthetic_response_utils.py
@@ -1,11 +1,58 @@
-#!/usr/bin/env python
+"""This module provides utilities for generating synthetic responses to survey questions,
+focusing initially on follow-up questions, using a specified persona and a Large Language Model (LLM).
 
+Classes:
+    SyntheticResponder: A class for generating synthetic responses to survey questions.
+    - Inputs:
+        - persona (optional): TODO object describing the characteristics of the persona to emulate.
+        - get_question_function (optional, callable): a helper function to retrieve question(s) from an API / data file.
+        - model_name (str): The name of the LLM to use. Fefaults to "gemini-1.5-flash".
+
+    - Methods:
+        - instantiate_llm: Initialises a VertexAI instance, using the model specified in the class.
+        - construct_prompt: Constructs a prompt for answering a follow-up question. 
+                            Requires arguments of 'body', a dictionary containing contextual information about 
+                            the survey response, and 'followup', a string containing the question to be answered.
+        - answer_followup: Gets the LLM's response to the follow-up question.
+                           Requires arguments of 'prompt', a PromptTemplate object constructed to have the LLM
+                           respond to the question in the given persona, and 'body', a dictionary containing contextual
+                           information about the survey response.
+
+Typical usage example:
+    ```python
+    from survey_assist_utils.synthetic_responses.synthetic_response_utils import SyntheticResponder
+
+    EXAMPLE_BODY = {"job_description": "Bake bread, cakes, pastries", 
+                    "job_title": "baker", 
+                    "industry_descr": "small scale, independently owned bakery"
+                    }
+
+    def get_question_example(body):
+        # In this example, we return the same quetion each time,
+        #  without considering the context of the survey response.
+        return "Is your business better described as wholesale or retail?"
+
+    SR = SyntheticResponder(persona=None, 
+                            get_question_function=get_question_example, 
+                            model_name="gemini-1.5-flash")
+
+    follow_up_question = SR.get_question_function(EXAMPLE_BODY)
+
+    prompt_to_answer_followup = SR.construct_prompt(EXAMPLE_BODY, follow_up_question)
+    answer_to_followup_question = SR.answer_followup(prompt_to_answer_followup, EXAMPLE_BODY)
+    ```
+
+
+"""
 import json
-
 import requests
 from langchain_google_vertexai import VertexAI
+from langchain.chains.llm import LLMChain
 from typing import Tuple, Optional
-from .prompts import _persona_prompt, _reminder_prompt
+from langchain.output_parsers import PydanticOutputParser
+from .prompts import make_followup_answer_prompt_pydantic
+from .response_models import FollowupAnswerResponse
+
 
 class SyntheticResponder:
     """
@@ -13,12 +60,11 @@ class SyntheticResponder:
     particularly follow-up questions, using a specified persona and a Large Language Model (LLM).
 
     Attributes:
+        get_question_function (optional, callable): A function that retrieves the follow-up question from an API.
+                                                    Defaults to None.
         persona (optional): A dictionary describing the demographic characteristics of the persona
                            the LLM should emulate. 
                            Defaults to None.
-        get_question_function (callable): A function that retrieves the follow-up question from an API.
-                                          It should take a parameter 'body' as a first argument, although
-                                          it can accept additional arguments.
         model_name (str): The name of the LLM to use. 
                           Defaults to "gemini-1.5-flash".
         llm: An instance of the LLM (currently VertexAI) used for generating responses.
@@ -34,9 +80,9 @@ class SyntheticResponder:
     """
 
     def __init__(self, 
-                 get_question_function: callable,
+                 get_question_function: Optional[callable] = None,
                  persona: Optional[dict] = None, 
-                 model_name="gemini-1.5-flash"):
+                 model_name: str="gemini-1.5-flash"):
         self.persona = persona
         self.get_question_function = get_question_function
         self.model_name = model_name
@@ -60,13 +106,35 @@ class SyntheticResponder:
         if type(body) is str:
             body = json.load(body)
         if type(followup) is str:
-            return _persona_prompt(self.persona) + _reminder_prompt(body, followup)
+            return make_followup_answer_prompt_pydantic(
+                persona=self.persona,
+                request_body=body,
+                followup_question=followup
+            )
         else: 
             raise ValueError("No follow-up question provided.")
 
 
-    def answer_followup(self, prompt: str) -> str:
+    def answer_followup(self, prompt: str, body: dict | str) -> str:
         """Gets the LLM's response to the followup question,
         as specified in the constructed prompt.
         """
-        return self.llm.invoke(prompt)
+        if type(body) not in (dict, str):
+            raise TypeError(
+                "'body' argument must be either a dictionary or a (string) path to a JSON file"
+            )
+        if type(body) is str:
+            body = json.load(body)
+        call_dict = body.copy()
+        call_dict["followup_question"] = prompt.partial_variables["followup_question"]
+        chain = LLMChain(llm=self.llm, prompt=prompt)
+        response = chain.invoke(call_dict, return_only_outputs=True)
+        parser = PydanticOutputParser(  # type: ignore # Suspect langchain ver bug
+            pydantic_object=FollowupAnswerResponse
+        )
+        try:
+            validated_answer = parser.parse(response["text"]).answer
+        except ValueError as parse_error:
+            logger.exception(parse_error)
+            logger.warning("Failed to parse response:\n%s", response["text"])
+        return validated_answer
