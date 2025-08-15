302777906202be624926b6f9258b4752b417ff7b
peter-spencer-ons <peter.spencer@ext.ons.gov.uk>
2025-08-15 14:41:28 +0000

    chore(one prompt eval): prepare script. missing docstrings

--- scripts/one_prompt_evaluation.py
+++ scripts/one_prompt_evaluation.py
@@ -20,25 +20,42 @@ def parse_args():
         type=str,
         help="relative path to the CSV file containing clerically coded data",
     )
-    # parser.add_argument(
-    #     "output_folder",
-    #     type=str,
-    #     help="relative path to the output folder location (will be created if it doesn't exist)",
-    # )
+    parser.add_argument(
+        "--prepared_data_columns",
+        "-pcol",
+        type=list,
+        default=['unique_id', 'sic_section', 'sic2007_employee',
+        'sic2007_self_employed', 'sic_ind1', 'sic_ind2', 'sic_ind3',
+        'sic_ind_code_flag', 'soc2020_job_title', 'soc2020_job_description',
+        'sic_ind_occ1', 'sic_ind_occ2', 'sic_ind_occ3', 'sic_ind_occ_flag',
+        'semantic_search_results', 'final_sic_code', 'sic_candidates'],
+        help="""A list of all columns from the prepared data to be included for
+        the evaluation (optional, default: list[str])""",
+    )
+    parser.add_argument(
+        "--clerical_data_columns",
+        "-ccol",
+        type=list,
+        default=['unique_id', 'Not_Codeable', 'Four_Or_More', 'SIC_Division',
+        'num_answers', 'All_Clerical_codes', 'Match_5_digits',
+        'Match_3_digits', 'Match_2_digits', 'Unambiguous'],
+        help="""A list of all columns from the clerically coded data to be
+        included for the evaluation (optional, default: list[str])""",
+    )
+    parser.add_argument(
+        "--test_configuration",
+        "-t",
+        type=str,
+        default="MM",
+        help="""test configuration; specify the type one-to-one, one-to-many,
+        many-to-one, many-to-many , select `OO`, `OM`, `MO`, or `MM`. Defaults to `MM`""",
+    )
     # parser.add_argument(
     #     "--output_shortname",
     #     "-n",
     #     type=str,
     #     default="STG1",
     #     help="output filename prefix for easy identification (optional, default: STG1)",
-    # )
-    # parser.add_argument(
-    #     "--batch_size",
-    #     "-b",
-    #     type=int,
-    #     default=200,
-    #     help="save the output every X rows, as a checkpoint that can be used to restart the "
-    #     "processing job if needed (optional, default: 200)",
     # )
     # parser.add_argument(
     #     "--restart",
@@ -88,72 +105,67 @@ def parse_clerical_code(candidates_str: str):
     except Exception as e:
         raise
 
-
+# Get prepared data
 def get_prepared_data(ss_df_path):
     return pd.read_parquet(ss_df_path)
 
+# Get clerically coded data
 def get_cc_data(cc_df_path):
-    return pd.read_csv(cc_df_path)
+    return pd.read_csv(cc_df_path) # Double check if that is CSV or parquet
 
-# 1. Load your prepared data
-# my_dataframe = pd.read_parquet("data/stage_2_one_prompt_20.parquet")
-# my_dataframe = pd.read_parquet("data/stage_2_one_prompt_2k.parquet")
+# Merge two datasets, preparing for the evaluation
+def merge_datasets(prepared_data, cc_data, prepared_data_columns, clerical_data_columns):
+    return prepared_data[prepared_data_columns].merge(cc_data[clerical_data_columns], left_on="unique_id", right_on="unique_id", how = "left")
 
-# # 2. Get clerically coded data
-# cc_data = pd.read_csv("data/tlfs_2k_eval_set.csv")
+# Unpack sic_candidates into new individual columns
+def sic_candidates_individual(merged_data):
+    for i in range(0,10):
+        merged_data['sic_candidate_' + str(i+1)] = merged_data['sic_candidate_list'].apply(lambda x: x[i].sic_code if len(x)>i else '')
 
-# # 3. Merge datasets
-# merged_data = my_dataframe[['unique_id', 'sic_section', 'sic2007_employee', 'sic2007_self_employed',
-#     'sic_ind1', 'sic_ind2', 'sic_ind3', 'sic_ind_code_flag',
-#     'soc2020_job_title', 'soc2020_job_description', 'sic_ind_occ1',
-#     'sic_ind_occ2', 'sic_ind_occ3', 'sic_ind_occ_flag',
-#     'semantic_search_results', 'final_sic_code', 'sic_candidates']].merge(cc_data[['unique_id', 'Not_Codeable',
-#     'Four_Or_More', 'SIC_Division', 'num_answers', 'All_Clerical_codes',
-#     'Match_5_digits', 'Match_3_digits', 'Match_2_digits', 'Unambiguous']], left_on="unique_id", right_on="unique_id", how = "left")
+    for i in range(0,10):
+        merged_data['sic_candidate_score_' + str(i+1)] = 0.5
 
-# # 4. Parse sic_candidates from LLM output
-# merged_data['sic_candidate_list'] = merged_data['sic_candidates'].apply(parse_sic_candidates)
+# Unpack CC labels
+def unpack_cc(merged_data):
+    merged_data['cc_list'] = merged_data['All_Clerical_codes'].apply(parse_clerical_code)
 
-# # 5. Unpack sic_candidates into new individual columns
-# for i in range(0,10):
-#     merged_data['sic_candidate_' + str(i+1)] = merged_data['sic_candidate_list'].apply(lambda x: x[i].sic_code if len(x)>i else '')
+    for i in range(0,5):
+        merged_data['cc_candidate_' + str(i+1)] = merged_data['cc_list'].apply(lambda x: x[i] if len(x)>i else '')
 
-# for i in range(0,10):
-#     merged_data['sic_candidate_score_' + str(i+1)] = 0.5
+def test_configuration(compare): # Future: parse how many columns form LLM compare to how many columns prepared by clerically coded.
+    # This example compares the top 3 model suggestions against 2 human-coded columns
+    # Add MO, OO
+    # should we allow comparing different sets, e.g. 3v3, 5v2 (currently we do 3v2)
+    """Choose from OO, MM, OM, MO, where O stands for One, M stands for Many.
+    First initial for clerically coded, second initial for LLM prepared data.
 
-# # 6. Unpack CC labels
-# merged_data['cc_list'] = merged_data['All_Clerical_codes'].apply(parse_clerical_code)
+    Args:
+        compare (str, optional): _description_. Defaults to "MM".
 
-# for i in range(0,5):
-#     merged_data['cc_candidate_' + str(i+1)] = merged_data['cc_list'].apply(lambda x: x[i] if len(x)>i else '')
-
-# # 7. Define the configuration for the test
-# # This example compares the top 3 model suggestions against 2 human-coded columns
-# col_config = ColumnConfig(
-#     model_label_cols=["sic_candidate_1", "sic_candidate_2"],
-#     model_score_cols=["sic_candidate_score_1", "sic_candidate_score_2"],
-#     clerical_label_cols=["cc_candidate_1", "cc_candidate_2"],
-#     id_col="unique_id",
-#     filter_unambiguous=True  # Only analyze unambiguous records in this case
-# )
-
-# # 3. Initialise the analyser with the DataFrame and config
-# analyzer = LabelAccuracy(df=merged_data, column_config=col_config)
-
-# # 4. Run the desired analysis
-# # Get detailed accuracy stats for full 5-digit matches
-# accuracy_stats = analyzer.get_accuracy(match_type="2-digit", extended=True) #match_type can be "2-digit"
+    Returns:
+        _type_: _description_
+    """
+    if compare == "MM":
+        col_config = ColumnConfig(
+            clerical_label_cols=["cc_candidate_1", "cc_candidate_2"],
+            model_label_cols=["sic_candidate_1", "sic_candidate_2"],
+            model_score_cols=["sic_candidate_score_1", "sic_candidate_score_2"],
+            id_col="unique_id",
+            filter_unambiguous=True  # Only analyze unambiguous records in this case
+        )
+    elif compare == "OM":
+        col_config = ColumnConfig(
+            clerical_label_cols=["cc_candidate_1"],
+            model_label_cols=["sic_candidate_1", "sic_candidate_2"],
+            model_score_cols=["sic_candidate_score_1", "sic_candidate_score_2"],
+            id_col="unique_id",
+            filter_unambiguous=True  # Only analyze unambiguous records in this case
+        )
+    return col_config
 
 # # Calculate the average suggestion quality
 # jaccard_score = analyzer.get_jaccard_similarity()
-
-# # Generate a confusion matrix heatmap
-# # analyzer.plot_confusion_heatmap(
-# #     human_code_col="CC_1",
-# #     llm_code_col="SA_1"
-# # )
-
-# print(f"Accuracy Stats: {accuracy_stats}")
+ 
 # print(f"Average Jaccard Score: {jaccard_score:.4f}")
 
 
@@ -161,6 +173,22 @@ def get_cc_data(cc_df_path):
 if __name__ == "__main__":
     args = parse_args()
 
+    # Read the data
     prepared_data = get_prepared_data(args.prepared_data)
     cc_data = get_cc_data(args.cc_data)
-    print(prepared_data)
+    
+    # Merge two datasets
+    merged_data = merge_datasets(prepared_data, cc_data, args.prepared_data_columns, args.clerical_data_columns)
+    
+    # Parse sic_candidates from LLM output
+    merged_data['sic_candidate_list'] = merged_data['sic_candidates'].apply(parse_sic_candidates)
+    
+    sic_candidates_individual(merged_data)
+    unpack_cc(merged_data)
+    col_config = test_configuration(args.test_configuration)
+    # Initialise the analyser with the DataFrame and config
+    analyzer = LabelAccuracy(df=merged_data, column_config=col_config)
+    # Run the desired analysis
+    # Get detailed accuracy stats for full 5-digit matches
+    accuracy_stats = analyzer.get_accuracy(match_type="2-digit", extended=True) #match_type can be "2-digit"
+    print(f"Accuracy Stats: {accuracy_stats}")
